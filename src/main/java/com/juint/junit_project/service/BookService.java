1b21c30ee7adaa1361188596fa5a6efcb3f3d6b0
Hea Jun Song <vmfmstor@gmail.com>
2022-12-06 16:19:55 +0900

    서비스 책 쓰기 추가

--- src/main/java/com/juint/junit_project/service/BookService.java
+++ src/main/java/com/juint/junit_project/service/BookService.java
@@ -1,8 +1,31 @@
 package com.juint.junit_project.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.juint.junit_project.domain.Book;
+import com.juint.junit_project.domain.BookRepository;
+import com.juint.junit_project.web.dto.BookReqSaveDto;
+import com.juint.junit_project.web.dto.BookResDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 1. 책 쓰기
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookResDto save(BookReqSaveDto dto) {
+        Book book = bookRepository.save(dto.toEntity());
+        return new BookResDto().toDto(book);
+    }
+
+    // 2. 책 목록
+    // 3. 책 상세
+    // 4. 책 삭제
+    // 5. 책 수정
 
 }
